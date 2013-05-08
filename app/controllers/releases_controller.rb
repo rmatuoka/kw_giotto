7587b69235129c1e792206e93a2aeac9c1d33c6f
Bruno Alves de Souza <brunoades@gmail.com>
2013-05-08 18:05:25 -0300

    98%

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -2,8 +2,13 @@ class ReleasesController < ApplicationController
   layout "template2013"
   before_filter :new_contact
   def index
-    @releases = Product.where(:novelty => true)
+    @products = Product.where(:novelty => true)
     @categories = Category.all
+  end
+  
+  def show_product
+    @product = Product.find_by_id(params[:id])
+    @product_images = @product.product_images
   end
 
 end
