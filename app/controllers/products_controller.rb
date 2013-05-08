c6865a19ef2da34ec1628e1f931789c4dad193d8
Bruno Alves de Souza <brunoades@gmail.com>
2013-05-08 20:33:41 -0300

    Menu de produtos funcionando, index de produtos carregando a primeira categoria

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,5 +14,16 @@ before_filter :new_contact
     @product = Product.find_by_id(params[:id])
     @product_images = @product.product_images
   end
+  
+  def show
+    @categories = Category.all
+    @category_id = Category.find_by_id(params[:id])
+    if !@category_id.blank?
+      render :index
+    else 
+      redirect_to products_path
+    end
+    
+  end
 
 end
