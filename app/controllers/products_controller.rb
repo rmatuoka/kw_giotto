0d3d705fb7709de2255c7f26244710f5e4a1af15
Bruno Alves de Souza <brunoades@gmail.com>
2013-05-14 19:35:20 -0300

    Solucionada duplicaidade nos produtos devido as imagens

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,7 +7,8 @@ before_filter :new_contact
   
   def show_category
     @category = Category.find_by_id(params[:id])
-    @products = Product.all(:joins=>"LEFT JOIN `product_images` ON `product_images`.`product_id` = `products`.`id`", :conditions => ["products.category_id=?",@category.id]) 
+    @products = @category.products
+
   end
   
   def show_product
