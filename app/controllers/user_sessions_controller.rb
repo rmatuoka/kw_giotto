e9ee7532d0c381611c139031292cecddb26bc0df
Bruno Alves <bsouza@korewa.com.br>
2013-06-05 15:05:15 -0300

    Ajustes pt-4

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,18 +7,21 @@ class UserSessionsController < ApplicationController
  
   def create
     @user_session = UserSession.new(params[:user_session])
-    if @user_session.save
-     flash[:error] = "" 
+    @user_session.save do |login|
+      if login
+     flash[:error] = "Login realizado com sucesso"
+     redirect_to :controller => 'home', :action => 'index', :target => '_blank'
+      end
+    end
     else
-      flash[:error] = "Usuário e/ou senha inválidos!"
+      flash[:error] = "UsuÃ¡rio e/ou senha invÃ¡lidos!"
     end
   end
  
   def destroy
     @user_session = UserSession.find
     @user_session.destroy
-    flash[:notice] = "Sessão finalizada com sucesso."
+    flash[:notice] = "SessÃ£o finalizada com sucesso."
     redirect_to root_url
   end
-	
-end
+
