3f7b1b4fa6e2eff09cecc700f5c753d77c35ac4b
-== Zaba ==- <bsilva@korewa.com.br>
2013-08-02 15:26:26 -0300

    Ajuste dos Formularios

--- app/controllers/find_us_controller.rb
+++ app/controllers/find_us_controller.rb
@@ -1,6 +1,6 @@
 class FindUsController < ApplicationController
   layout "template2013"
-  before_filter :new_contact 
+  before_filter :new_contact
   def index
     redirect_to new_find_u_path
   end
@@ -11,10 +11,10 @@ class FindUsController < ApplicationController
   end
   
   def create
-    @contact = ::Find.new(params[:find])
-    if @contact.save
-      ContactMailer.send_findus_to_adm(@contact).deliver
-      redirect_to find_u_path(@contact), :notice => "Mensagem de sucesso."
+    @findcontact = ::Find.new(params[:post])
+    if @findcontact.save
+      ContactMailer.send_findus_to_adm(@findcontact).deliver
+      redirect_to find_u_path(@findcontact), :notice => "Mensagem de sucesso."
   
   else
     render :action => 'new'
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,21 +3,21 @@ class ContactMailer < ActionMailer::Base
   
   def send_representative_to_adm(contact)
     @contact = contact
-    mail(:to => "comercial@produtosgiotto.com.br",:bcc => "log@korewa.com.br", :subject => "Giotto - Novo Contato")
+    mail(:to => "comercial@produtosgiotto.com.br",:bcc => "log@korewa.com.br", :subject => "Giotto - Representante")
   end
   
   def send_contact_to_adm(contact)
     @contact = contact
-    mail(:to => "comercial@produtosgiotto.com.br",:bcc => "log@korewa.com.br", :subject => "Giotto - Novo Contato")
+    mail(:to => "comercial@produtosgiotto.com.br",:bcc => "log@korewa.com.br", :subject => "Giotto - Contato")
   end
   
   def send_findus_to_adm(contact)
     @contact = contact
-    mail(:to => "comercial@produtosgiotto.com.br",:bcc => "log@korewa.com.br", :subject => "Giotto - Novo Contato")
+    mail(:to => "comercial@produtosgiotto.com.br",:bcc => "log@korewa.com.br", :subject => "Giotto - Onde Encontrar")
   end
   
   def send_work_to_adm(contact)
     @contact = contact
-    mail(:to => "vagner@produtosgiotto.com.br",:bcc => "bsouza@korewa.com.br", :subject => "Giotto - Novo CV")
+    mail(:to => "vagner@produtosgiotto.com.br",:bcc => "bsouza@korewa.com.br", :subject => "Giotto - Trabalhe Conosco")
   end
 end
